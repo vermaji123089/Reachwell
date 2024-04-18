4f00a3365cf62d700ee101a111b22f3df607ae3e
MOHIT VERMA <66687560+vermaji123089@users.noreply.github.com>
2024-04-18 17:25:25 +0530

    meta chanegs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,31 @@
+import React from 'react';
+import Head from 'next/head';
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/Footer';
 
-
 export const metadata = {
   title: 'Reachwell Worldwide Express',
-  description: 'Global Logistics Partner  Logistics through innovation, dedication, and technology',
-}
+  description: 'Global Logistics Partner - Logistics through innovation, dedication, and technology',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* Add other meta tags for SEO as needed */}
+      </Head>
       <body>
         <Navbar />
         {children}
         <Footer />
       </body>
-    </html>
-  )
+    </>
+  );
 }
