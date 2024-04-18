9d68c54895a876db2cea59a6a04e820c5a72b450
MOHIT VERMA <66687560+vermaji123089@users.noreply.github.com>
2024-04-18 18:09:38 +0530

    meta changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,13 @@ import Head from 'next/head';
 import './globals.css';
 import Navbar from './components/Navbar/index';
 import Footer from './components/Footer/Footer';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export const metadata = {
   title: 'Reachwell Worldwide Express',
   description: 'Global Logistics Partner - Logistics through innovation, dedication, and technology',
+  author: 'Reachwell Worldwide Express',
+  keywords: 'global logistics, innovation, dedication, technology',
+  url: 'https://reachwell.vercel.app',
 };
 
 export default function RootLayout({
@@ -17,8 +20,13 @@ export default function RootLayout({
   return (
     <>
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <meta name="author" content={metadata.author} />
+        <meta name="keywords" content={metadata.keywords} />
+        <link rel="canonical" href={metadata.url} />
         {/* Add other meta tags for SEO as needed */}
       </Head>
       <body>
